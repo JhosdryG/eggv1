c26ef917b4a40aab4edc43853761bfe970db67ce
JhosdryG <jhosephgp@gmail.com>
2021-10-14 10:23:15 -0400

    add modal

---
+++ scripts/socialmodal.js
@@ -0,0 +1,8 @@
+const socialModal = document.getElementById("socialModal");
+
+if (localStorage.getItem("modal")) socialModal.classList.add("no-show");
+else localStorage.setItem("modal", "true");
+
+document.getElementById("closeModal").addEventListener("click", () => {
+  socialModal.classList.add("no-show");
+});
