c250f837b1464c64fe4b0aaacdffe3e378e54e1b
JhosdryG <jhosephgp@gmail.com>
2021-10-14 11:45:40 -0400

    change localstorage to sesionstorage

--- scripts/socialmodal.js
+++ scripts/socialmodal.js
@@ -1,7 +1,7 @@
 const socialModal = document.getElementById("socialModal");
 
-// if (localStorage.getItem("modal")) socialModal.classList.add("no-show");
-// else localStorage.setItem("modal", "true");
+if (sessionStorage.getItem("modal")) socialModal.classList.add("no-show");
+else sessionStorage.setItem("modal", "true");
 
 document.getElementById("closeModal").addEventListener("click", () => {
   socialModal.classList.add("no-show");
